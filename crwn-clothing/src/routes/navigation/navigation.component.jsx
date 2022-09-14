760a9cc3019e4e4728e5c03826eb5abe4b1e83be
hayatochida <h.c.soccer.72721@gmail.com>
2022-09-14 17:50:11 +0900

    Adding

---
+++ crwn-clothing/src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,46 @@
+import { Fragment, useContext } from "react";
+import { Outlet, Link } from "react-router-dom";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+import { NavigationContainer, LogoContainer, NavLinks, NavLink } from "./navigation.styles";
+
+
+const Navigation = () => {
+    const { currentUser, setCurrentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
+
+    const signOutHandler = async () => {
+        const res = await signOutUser();
+        setCurrentUser(null);
+    };
+    return (
+        <Fragment>
+            <NavigationContainer>
+                <LogoContainer to="/">
+                    <CrwnLogo className="logo" />
+                </LogoContainer>
+                <NavLinks>
+                    <NavLink to="/shop">
+                        Shop
+                    </NavLink>
+                    {currentUser ? (
+                        <NavLink as='span' onClick={signOutHandler}>SIGN OUT</NavLink>
+                    ) : (
+                        <NavLink to="/auth">
+                            SIGN IN
+                        </NavLink>
+                    )}
+                    <CartIcon />
+                </NavLinks>
+                {isCartOpen && <CartDropdown />}
+            </NavigationContainer>
+            <Outlet />
+        </Fragment>
+    );
+};
+
+export default Navigation;
