d057376c895b56452771988bb10bbae2cc1d1398
hayatochida <h.c.soccer.72721@gmail.com>
2022-09-14 18:48:07 +0900

    re re adding

---
+++ src/contexts/user.context.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { createContext } from "react";
+import { onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
+import { createUserDocumentFromAuth } from "../utils/firebase/firebase.utils";
+
+export const UserContext = createContext({
+    currentUser: null,
+    setCurrentUser: () => null,
+});
+
+export const UserProvider = ({ children }) => {
+    const [currentUser, setCurrentUser] = useState(null);
+    const value = { currentUser, setCurrentUser };
+
+   
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener((user) => {
+            if(user) {
+               createUserDocumentFromAuth(user);
+            }
+            setCurrentUser(user);
+        })
+        return unsubscribe;
+    }, []);
+
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+};
